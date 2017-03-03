9fb94259318595da469f20b3de53d14dbaa20fd2
ITLC <1740104885@qq.com>
2017-03-03 18:48:09 +0800

    CommunityFragment实例化布局

--- app/src/main/java/com/atguigu/shopping/community/communityfragment/CommunityFragment.java
+++ app/src/main/java/com/atguigu/shopping/community/communityfragment/CommunityFragment.java
@@ -1,11 +1,16 @@
 package com.atguigu.shopping.community.communityfragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.atguigu.shopping.R;
 import com.atguigu.shopping.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by 刘闯 on 2017/2/22.
@@ -13,22 +18,45 @@ import com.atguigu.shopping.base.BaseFragment;
 
 public class CommunityFragment extends BaseFragment {
 
-    private TextView textView;
+    @InjectView(R.id.ib_community_icon)
+    ImageButton ibCommunityIcon;
+    @InjectView(R.id.ib_community_message)
+    ImageButton ibCommunityMessage;
+    
+    @InjectView(R.id.view_pager)
+    ViewPager viewPager;
+   
 
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-
-        textView.setTextColor(Color.RED);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_community, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("发现");
+       
+    }
 
+    
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.ib_community_icon, R.id.ib_community_message})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_community_icon:
+                Toast.makeText(mContext, "图像", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_community_message:
+                Toast.makeText(mContext, "消息", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
