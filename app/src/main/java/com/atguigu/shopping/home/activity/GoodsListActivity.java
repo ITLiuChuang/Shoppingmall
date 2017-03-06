e62380f857a09caf4dcb2ea089970e7a8285ac46
ITLC <1740104885@qq.com>
2017-03-06 17:50:47 +0800

    设置综合排序价格筛选点击效果

--- app/src/main/java/com/atguigu/shopping/home/activity/GoodsListActivity.java
+++ app/src/main/java/com/atguigu/shopping/home/activity/GoodsListActivity.java
@@ -25,6 +25,7 @@ import android.widget.Toast;
 import com.alibaba.fastjson.JSON;
 import com.atguigu.shopping.R;
 import com.atguigu.shopping.app.GoodsInfoActivity;
+import com.atguigu.shopping.home.adapter.ExpandableListViewAdapter;
 import com.atguigu.shopping.home.adapter.GoodsListAdapter;
 import com.atguigu.shopping.home.adapter.HomeAdapter;
 import com.atguigu.shopping.home.bean.GoodsBean;
@@ -34,6 +35,7 @@ import com.atguigu.shopping.utils.Constants;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -204,6 +206,9 @@ public class GoodsListActivity extends AppCompatActivity {
     private int position;
     private GoodsListAdapter adapter;
     private int click_count;
+    private ArrayList<String> group;
+    private ArrayList<List<String>> child;
+    private ExpandableListViewAdapter expandableListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -278,7 +283,7 @@ public class GoodsListActivity extends AppCompatActivity {
 
     }
 
-    @OnClick({R.id.ib_goods_list_back, R.id.tv_goods_list_search, R.id.ib_goods_list_home, R.id.tv_goods_list_sort, R.id.tv_goods_list_price, R.id.tv_goods_list_select,R.id.ib_drawer_layout_back, R.id.ib_drawer_layout_confirm, R.id.rl_select_price, R.id.rl_select_recommend_theme, R.id.rl_select_type, R.id.btn_drawer_layout_cancel, R.id.btn_drawer_layout_confirm, R.id.btn_drawer_theme_cancel, R.id.btn_drawer_theme_confirm, R.id.btn_drawer_type_cancel, R.id.btn_drawer_type_confirm})
+    @OnClick({R.id.ib_goods_list_back, R.id.tv_goods_list_search, R.id.ib_goods_list_home, R.id.tv_goods_list_sort, R.id.tv_goods_list_price, R.id.tv_goods_list_select, R.id.ib_drawer_layout_back, R.id.ib_drawer_layout_confirm, R.id.rl_select_price, R.id.rl_select_recommend_theme, R.id.rl_select_type, R.id.btn_drawer_layout_cancel, R.id.btn_drawer_layout_confirm, R.id.btn_drawer_theme_cancel, R.id.btn_drawer_theme_confirm, R.id.btn_drawer_type_cancel, R.id.btn_drawer_type_confirm})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.ib_goods_list_back:
@@ -409,7 +414,65 @@ public class GoodsListActivity extends AppCompatActivity {
         llSelectRoot.setVisibility(View.GONE);
         llPriceRoot.setVisibility(View.GONE);
         llThemeRoot.setVisibility(View.GONE);
+
+        //初始化ExpandableListView
+        initExpandableListView();
     }
 
+    private void initExpandableListView() {
+        //创建集合
+        group = new ArrayList<>();
+        child = new ArrayList<>();
 
+        //添加数据
+        addInfo("全部", new String[]{});
+        addInfo("上衣", new String[]{"古风", "和风", "lolita", "日常"});
+        addInfo("下装", new String[]{"日常", "泳衣", "汉风", "lolita", "创意T恤"});
+        addInfo("外套", new String[]{"汉风", "古风", "lolita", "胖次", "南瓜裤", "日常"});
+
+        //设置适配器
+        expandableListViewAdapter = new ExpandableListViewAdapter(this, group, child);
+        expandableListView.setAdapter(expandableListViewAdapter);
+
+        //设置孩子的点击事件
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                //把位置传入适配器中
+                expandableListViewAdapter.isChildSelectable(groupPosition, childPosition);
+                //刷新
+                expandableListViewAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        // 这里是控制如果列表没有孩子菜单不展开的效果
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                if (child.get(groupPosition).isEmpty()) {
+                    return true;
+                } else {
+                    return false;
+
+                }
+            }
+        });
+
+    }
+
+    /**
+     * 添加数据信息
+     *
+     * @param g
+     * @param c
+     */
+    private void addInfo(String g, String[] c) {
+        group.add(g);
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < c.length; i++) {
+            list.add(c[i]);
+        }
+        child.add(list);
+    }
 }
